35596ee01d34b264a6aa7a81945f2e5bfae94d09
Tiago Dias <freshman21@gmail.com>
2018-12-19 21:29:23 +0000

    feat: form validation functions

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -58,12 +58,113 @@ class RegisterBox extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = { username: "", email: "", password: "", errors: [] };
     }
 
-    submitRegister(e) { }
+    //Add New Error Object to the array {elm: msg}
+    showValidationErr(elm, msg) {
+        this.setState((prevState) => ({
+            errors: [
+                ...prevState.errors, {
+                    elm,
+                    msg
+                }
+            ]
+        }));
+    }
+    //Remove a specific element from the array 
+    clearValidationErr(elm) {
+        this.setState((prevState) => {
+            let newArr = [];
+            //Add all elements from the prev array to the new one that has a different element
+            for (let err of prevState.errors) {
+                if (elm !== err.elm) {
+                    newArr.push(err);
+                }
+            }
+            return { errors: newArr };
+        });
+    }
+
+    //Update Username, password, and email on change event 
+    onUsernameChange(e) {
+        this.setState({ username: e.target.value });
+        //We want to clear the error when ever the user type something new 
+        this.clearValidationErr("username");
+    }
+
+    onEmailChange(e) {
+        this.setState({ email: e.target.value });
+        this.clearValidationErr("email");
+    }
+
+    onPasswordChange(e) {
+        this.setState({ password: e.target.value });
+        this.clearValidationErr("password");
+
+        //By default the state is weak
+        this.setState({ pwdState: "weak" });
+        if (e.target.value.length > 8) {
+            this.setState({ pwdState: "medium" });
+        }
+        if (e.target.value.length > 12) {
+            this.setState({ pwdState: "strong" });
+        }
+    }
+
+    submitRegister(e) { 
+        //Check for all input fields and show errors if empty (you can implement other cases!)
+        if (this.state.username === "") {
+            this.showValidationErr("username", "Username Cannot be empty!");
+        }
+        if (this.state.email === "") {
+            this.showValidationErr("email", "Email Cannot be empty!");
+        }
+        if (this.state.password === "") {
+            this.showValidationErr("password", "Password Cannot be empty!");
+        }
+    }
 
     render() {
+
+        //NULL by default (help us check when rendering)
+        let usernameErr = null,
+            passwordErr = null,
+            emailErr = null;
+        //Loop and find which ones has the error
+        for (let err of this.state.errors) {
+            //Assign the validation error message 
+            if (err.elm === "username") {
+                usernameErr = err.msg;
+            }
+            if (err.elm === "password") {
+                passwordErr = err.msg;
+            }
+            if (err.elm === "email") {
+                emailErr = err.msg;
+            }
+            //No (else if or else) statements cause we need to check for all possible elements
+        }
+
+        //states are by default false = (do not render)
+        let pwdWeak = false,
+            pwdMedium = false,
+            pwdStrong = false;
+        //Weak password set onlt the pwdWeak to true, cause render only the first bar 
+        if (this.state.pwdState === "weak") {
+            pwdWeak = true;
+        } else if (this.state.pwdState === "medium") {
+            //Medium pwd then render the weak and medium bars 
+            pwdWeak = true;
+            pwdMedium = true;
+        } else if (this.state.pwdState === "strong") {
+            //Strong, render all the previoud bars 
+            pwdWeak = true;
+            pwdMedium = true;
+            pwdStrong = true;
+        }
+
+
         return (
             <div className="inner-container">
                 <div className="header">
@@ -77,12 +178,26 @@ class RegisterBox extends React.Component {
                             type="text"
                             name="username"
                             className="login-input"
-                            placeholder="Username" />
+                            placeholder="Username" 
+                            onChange={this
+                                .onUsernameChange
+                                .bind(this)}/>
+                        <small className="danger-error">{usernameErr
+                            ? usernameErr
+                            : ""}
+                        </small>
                     </div>
 
                     <div className="input-group">
                         <label htmlFor="email">Email</label>
-                        <input type="text" name="email" className="login-input" placeholder="Email" />
+                        <input type="text" name="email" className="login-input" placeholder="Email" 
+                            onChange={this
+                                .onEmailChange
+                                .bind(this)}/>
+                        <small className="danger-error">{emailErr
+                            ? emailErr
+                            : ""}
+                        </small>
                     </div>
 
                     <div className="input-group">
@@ -91,7 +206,28 @@ class RegisterBox extends React.Component {
                             type="password"
                             name="password"
                             className="login-input"
-                            placeholder="Password" />
+                            placeholder="Password" 
+                            onChange={this
+                                .onPasswordChange
+                                .bind(this)}/>
+                        <small className="danger-error">{passwordErr
+                            ? passwordErr
+                            : ""}
+                        </small>
+                        {this.state.password && <div className="password-state">
+                            <div
+                                className={"pwd pwd-weak " + (pwdWeak
+                                    ? "show"
+                                    : "")}></div>
+                            <div
+                                className={"pwd pwd-medium " + (pwdMedium
+                                    ? "show"
+                                    : "")}></div>
+                            <div
+                                className={"pwd pwd-strong " + (pwdStrong
+                                    ? "show"
+                                    : "")}></div>
+                        </div>}
                     </div>
                     <button
                         type="button"
@@ -122,6 +258,7 @@ class App extends React.Component {
     showRegisterBox() {
         this.setState({ isRegisterOpen: true, isLoginOpen: false });
     }
+
 
     render() {
 
